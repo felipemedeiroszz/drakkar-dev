fad2949a681661609d0f15bcbe48d3b7d29b7563
brassz <brasszgc@gmail.com>
2025-07-31 17:24:27 -0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -7,5 +7,5 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 // Cliente para server-side
 export const createServerClient = () => {
-  return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!)
+  return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
 }
